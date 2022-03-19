56cf33c8447c5ab90b72f011e7c50f4ae8fa1b9e
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-03-19 18:29:56 +0400

    update Speedtest.py

--- Yukki/Plugins/Speedtest.py
+++ Yukki/Plugins/Speedtest.py
@@ -12,39 +12,39 @@ __MODULE__ = "Speedtest"
 __HELP__ = """
 
 /speedtest 
-- Check Server Latency and Speed.
+- Server gecikmə və sürətini yoxlayın.
 
-"""
+ """
 
 
 @app.on_message(filters.command("speedtest") & ~filters.edited)
 async def statsguwid(_, message):
-    m = await message.reply_text("Running Speed test")
+    m = await message.reply_text("Qaçış sürəti testi")
     try:
         test = speedtest.Speedtest()
         test.get_best_server()
-        m = await m.edit("Running Download SpeedTest")
+        m = await m.edit("Endirmə SpeedTest-in icrası")
         test.download()
-        m = await m.edit("Running Upload SpeedTest")
+        m = await m.edit("Yükləmə Sürət Testi işləyir")
         test.upload()
         test.results.share()
         result = test.results.dict()
     except Exception as e:
         return await m.edit(e)
-    m = await m.edit("Sharing SpeedTest Results")
+    m = await m.edit("SpeedTest Nəticələrinin Paylaşılması")
     path = wget.download(result["share"])
 
-    output = f"""**Speedtest Results**
+    output = f"""**Sürət Test Nəticələri**
     
-<u>**Client:**</u>
-**__ISP:__** {result['client']['isp']}
+<u>**Müştəri:**</u>
+ **__ISP:__** {result['client']['isp']}
 **__Country:__** {result['client']['country']}
   
 <u>**Server:**</u>
-**__Name:__** {result['server']['name']}
-**__Country:__** {result['server']['country']}, {result['server']['cc']}
+ **__Ad:__** {result['server']['name']}
+**__Ölkə:__** {result['server']['country']}, {result['server']['cc']}
 **__Sponsor:__** {result['server']['sponsor']}
-**__Latency:__** {result['server']['latency']}  
+**__Gecikmə:__** {result['server']['latency']}  
 **__Ping:__** {result['ping']}"""
     msg = await app.send_photo(
         chat_id=message.chat.id, photo=path, caption=output
