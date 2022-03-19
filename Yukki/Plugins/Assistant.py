5ef819ca6197d7543494487c0ad715b6a684c707
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-03-19 11:22:20 +0400

    update Assistant.py

--- Yukki/Plugins/Assistant.py
+++ Yukki/Plugins/Assistant.py
@@ -11,41 +11,41 @@ from Yukki import ASSISTANT_PREFIX, SUDOERS, app, random_assistant
 from Yukki.Database import get_assistant, save_assistant
 from Yukki.Utilities.assistant import get_assistant_details
 
-__MODULE__ = "Assistant"
+__MODULE__ = "köməkçi"
 __HELP__ = f"""
 
 
 /checkassistant
-- Check the alloted assistant of your chat
+- Söhbətinizin ayrılmış köməkçisini yoxlayın
 
 
-**Note:**
-- Only for Sudo Users
+**Qeyd:**
+ - Yalnız Sudo İstifadəçiləri üçün
 
-{ASSISTANT_PREFIX[0]}block [ Reply to a User Message] 
-- Blocks the User from Assistant Account.
+{ASSISTANT_PREFIX[0]}blok [ İstifadəçi Mesajına Cavab Verin]
+ - İstifadəçini köməkçi hesabından bloklayır.
 
-{ASSISTANT_PREFIX[0]}unblock [ Reply to a User Message] 
-- Unblocks the User from Assistant Account.
+{ASSISTANT_PREFIX[0]}blokdan çıxarın [ İstifadəçi Mesajına Cavab Verin]
+ - İstifadəçini Assistent Hesabından blokdan çıxarır.
 
-{ASSISTANT_PREFIX[0]}approve [ Reply to a User Message] 
-- Approves the User for DM.
+{ASSISTANT_PREFIX[0]}təsdiq edin [ İstifadəçi Mesajına Cavab Verin]
+ - İstifadəçini DM üçün təsdiqləyir.
 
-{ASSISTANT_PREFIX[0]}disapprove [ Reply to a User Message] 
-- Disapproves the User for DM.
+{ASSISTANT_PREFIX[0]}təsdiq et [ İstifadəçi Mesajına Cavab Verin]
+ - İstifadəçini DM üçün təsdiq etmir.
 
-{ASSISTANT_PREFIX[0]}pfp [ Reply to a Photo] 
-- Changes Assistant account PFP.
+{ASSISTANT_PREFIX[0]}pfp [ Fotoya cavab verin]
+ - Köməkçi hesab PFP-ni dəyişir.
 
-{ASSISTANT_PREFIX[0]}bio [Bio text] 
-- Changes Bio of Assistant Account.
+ {ASSISTANT_PREFIX[0]}bio [Bio mətn]
+ - Assistant Hesabının Tərkibi dəyişdirilir.
 
-/changeassistant [ASS NUMBER]
-- Change the previoius alloted assistant to new one.
+/changeassistant [ASS NÖMRƏSİ]
+ - Əvvəlki köməkçini yenisi ilə dəyişdirin.
 
-/setassistant [ASS NUMBER or Random]
-- Set a assistant account for chat. 
-"""
+/setassistant [ASS NÖMRƏSİ və ya Təsadüfi]
+ - Söhbət üçün köməkçi hesabı təyin edin.
+ """
 
 
 ass_num_list = ["1", "2", "3", "4", "5"]
@@ -53,7 +53,7 @@ ass_num_list = ["1", "2", "3", "4", "5"]
 
 @app.on_message(filters.command("changeassistant") & filters.user(SUDOERS))
 async def assis_change(_, message: Message):
-    usage = f"**Usage:**\n/changeassistant [ASS_NO]\n\nSelect from them\n{' | '.join(ass_num_list)}"
+    usage = f"**İstifadə:**\n/dəyişiklik köməkçisi [ASS_NO]\n\nOnlardan seçin\n{' | '.join(ass_num_list)}"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     num = message.text.split(None, 1)[1].strip()
@@ -63,7 +63,7 @@ async def assis_change(_, message: Message):
     _assistant = await get_assistant(message.chat.id, "assistant")
     if not _assistant:
         return await message.reply_text(
-            "No Pre-Saved Assistant Found.\n\nYou can set Assistant Via /setassistant"
+            "Qabaqcadan saxlanmış köməkçi tapılmadı./setassistant vasitəsilə Assistent təyin edə bilərsiniz"
         )
     else:
         ass = _assistant["saveassistant"]
@@ -72,7 +72,7 @@ async def assis_change(_, message: Message):
     }
     await save_assistant(message.chat.id, "assistant", assis)
     await message.reply_text(
-        f"**Changed Assistant**\n\nChanged Assistant Account from **{ass}** to Assistant Number **{ass_num}**"
+        f"**Dəyişən Assistent**\n\nAssistent Hesabı **{ass}**-dan **{ass_num}**-a dəyişdirildi"
     )
 
 
@@ -81,7 +81,7 @@ ass_num_list2 = ["1", "2", "3", "4", "5", "Random"]
 
 @app.on_message(filters.command("setassistant") & filters.user(SUDOERS))
 async def assis_change(_, message: Message):
-    usage = f"**Usage:**\n/setassistant [ASS_NO or Random]\n\nSelect from them\n{' | '.join(ass_num_list2)}\n\nUse 'Random' to set random Assistant"
+    usage = f"**İstifadə:**\n/setassistant [ASS_NO və ya Random]\n\nOnlardan seçin\n{' | '.join(ass_num_list2)}\n\nTəsadüfi Assistent qurmaq üçün 'Təsadüfi' istifadə edin"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     query = message.text.split(None, 1)[1].strip()
